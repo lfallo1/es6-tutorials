bacc2bd121eee451aec5d8853bf7873a0a81c8cf
Lance Fallon <fallon.lance@gmail.com>
2018-04-02 15:53:59 -0400

    http primitive to fetch

--- src/http.js
+++ src/http.js
@@ -1,7 +1,14 @@
 export class Http {
     static fetchData(url) {
         return new Promise((resolve, reject) => {
-            const HTTP = new XMLHttpRequest();
+
+            fetch(url, {method: "GET"})
+                .then(response => response.json())
+                .then(json => resolve(json),
+                    err => reject('Something went wrong'));
+
+            /*
+                        const HTTP = new XMLHttpRequest();
             HTTP.open('GET', url);
             HTTP.onreadystatechange = function() {
                 if (HTTP.readyState == XMLHttpRequest.DONE && HTTP.status == 200) {
@@ -13,6 +20,8 @@ export class Http {
                 }
             };
             HTTP.send();
+             */
+
         });
     }
 }
